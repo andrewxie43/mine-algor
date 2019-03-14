1e4c5b3194bb62e3a8d444b26549eaf30dee831b
Andrew X <andrewxie43@gmail.com>
2019-03-14 11:55:18 -0700

    Updated GUI: Buttons return x and y for dig or flag.

--- fieldButton.java
+++ fieldButton.java
@@ -10,19 +10,20 @@ public class fieldButton implements ActionListener{
   int coordX;
   int coordY;
 
+
+
   public fieldButton(int cx, int cy, int fx, int fy){
     coordX = cx;
     coordY = cy;
 
     fieldX = fx;
     fieldY = fy;
+
+    JButton b=new JButton("(" + Integer.toString(fieldX) + "," + Integer.toString(fieldY) + ")");//creating instance of JButton
+    b.setBounds(coordX,coordY,90,90);//x axis, y axis, width, height
   }
 
-  @Override
-  public String actionPerformed(ActionEvent arg0) {
-    return ;
-
-  }
+  
 
 
 
--- gui.java
+++ gui.java
@@ -3,18 +3,41 @@ import java.awt.event.*;
 import javax.swing.*;
 
 
-public class gui implements ActionListener {
+public class gui{
+
+  public static int clickedX;
+  public static int clickedY;
+
+  public static JFrame f = new JFrame();
+
   public static void main(String[] args) { //does 8x8 only, configure for modularity
-    JFrame f=new JFrame();//creating instance of JFrame
+    //creating instance of JFrame
 
     int xCoord = 0;
     int yCoord = 0;
+
+
+
     for(int x = 45; x < 755; x+=90){
+
+      final int xCoordFinal = xCoord;
+
+
+
       for(int y = 45; y < 755; y+=90){
-        JButton b=new JButton("(" + Integer.toString(xCoord) + "," + Integer.toString(yCoord) + ")");//creating instance of JButton
+        final int yCoordFinal = yCoord;
+        JButton b=new JButton(new AbstractAction("(" + Integer.toString(xCoord) + "," + Integer.toString(yCoord) + ")") {//creating instance of JButton
+          @Override
+          public void actionPerformed(ActionEvent e) { //field button clicked
+            setClicked(xCoordFinal,yCoordFinal);
+            System.out.println("x= " + xCoordFinal);
+            System.out.println("y= " + yCoordFinal);
+          }
+        });
+
         b.setBounds(x,y,90,90);//x axis, y axis, width, height
 
-        
+
 
         f.add(b);//adding button in JFrame
         yCoord++;
@@ -22,12 +45,20 @@ public class gui implements ActionListener {
       yCoord = 0;
       xCoord++;
     }
+
+    /*
+    JLabel xlab = new JLabel("X Coord: " + clickedX); //Used to test buttons, implemented in setClickedX/Y
+    xlab.setBounds(900,135,90,45);
+    f.add(xlab);
+
+    JLabel ylab = new JLabel("Y Coord: " + clickedY);
+    ylab.setBounds(900,180,90,45);
+    f.add(ylab);
+    */
+
+
     JToggleButton d=new JToggleButton("Dig"); //dig button
     d.setBounds(900,45,90,45);
-
-
-
-
     f.add(d); //make mutually exclusive
 
     JToggleButton fl=new JToggleButton("Flag"); //flag button
@@ -45,4 +76,31 @@ public class gui implements ActionListener {
     f.setVisible(true);//making the frame visible
     f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   }
+
+
+
+  public String clicked(){
+    return Integer.toString(clickedX) + " " + Integer.toString(clickedY);
+  }
+
+  public static void setClicked(int x, int y){
+    //clickedX = x;
+    //clickedY = y;
+    JLabel xlab = new JLabel("X Coord: " + x); //Used to test buttons
+    xlab.setBounds(900,135,90,45);
+    f.add(xlab);
+
+    JLabel ylab = new JLabel("Y Coord: " + y);
+    ylab.setBounds(900,180,90,45);
+    f.add(ylab);
+
+    f.repaint()
+;  }
+
+
+
+
+
+
+
 }
